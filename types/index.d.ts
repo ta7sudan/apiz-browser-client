8d1a7046ad88dc48ed026d56defea812fd3ce5d9
ta7sudan <ta7sudan@gmail.com>
2019-02-05 20:32:06 +0800

    update

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,6 +3,6 @@ interface Options {
 	afterResponse?(resData: any, status: string, xhr: XMLHttpRequest, url: string, reqData: any): void,
 	complete?(resData: any, xhr: XMLHttpRequest, url: string, reqData: any): void,
 	retry?: number
-};
+}
 
 export default function (options?: Options): object;
